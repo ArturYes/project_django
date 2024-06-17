95136a0968ea6a2e51c5d53d701762e07673f049
artur <youzen0you@gmail.com>
2024-06-17 16:28:46 +0400

    20.2 - Added templates. Created a controller and product template. Expanded the main page. Created, added tag and filter.

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 
 from apps.catalog.apps import CatalogConfig
-from apps.catalog.views import index, contacts
+from apps.catalog.views import index, contacts, product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contacts, name='contacts')
+    path('', index, name='catalog'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>/', product, name='product')
 ]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 
+from apps.catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {'title': 'Каталог'}
+
+    data = {'objects_list': Product.objects.all()}
+    context.update(data)
+
+    return render(request, 'catalog/catalog.html', context)
 
 
 def contacts(request):
+    context = {'title': 'Каталог'}
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'Имя: {name}, Номер: {phone}, Сообщение: {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    context = {
+        'title': 'Продукт',
+        'object': Product.objects.get(pk=pk)
+    }
+
+    return render(request, 'catalog/product.html', context)
+
